7ea2e0e8c50eeade88ab7b95a4f7a8263222f1ec
Athan Clark <athan.clark@gmail.com>
2017-08-25 12:34:53 -0600

    working example

--- example/src/Main.purs
+++ example/src/Main.purs
@@ -1,11 +1,13 @@
 module Main where
 
 import Prelude
+import Control.Monad.Rec.Class (forever)
 import Control.Monad.Aff (delay, runAff)
 import Control.Monad.Eff (Eff)
 import Control.Monad.Eff.Class (liftEff)
 import Control.Monad.Eff.Console (CONSOLE, log, logShow, errorShow)
 
+import Data.Time.Duration (Milliseconds (..))
 import Data.Maybe (Maybe (..))
 import Data.Tuple (Tuple (..))
 import Data.URI (Authority (..), Host (..), Port (..))
@@ -33,9 +35,9 @@ publisher :: Eff _ Unit
 publisher =
   registerPublisher socket $ \send -> do
     b <- fromArray [73,75,70]
-    void $ runAff errorShow logShow $ forever $ do
-      delay 1000
-      liftEff $ send "foo" "asd"
+    void $ runAff errorShow (const $ pure unit) $ forever $ do
+      delay $ Milliseconds 1000.0
+      liftEff $ send "foo" b
 
 subscriber :: Eff _ Unit
 subscriber =
