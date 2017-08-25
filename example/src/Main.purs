0716b966c28435d94e4dc09ce744bdd4be427561
Athan Clark <athan.clark@gmail.com>
2017-08-25 11:54:13 -0600

    producer / worker looks good

--- example/src/Main.purs
+++ example/src/Main.purs
@@ -8,6 +8,7 @@ import Data.Maybe (Maybe (..))
 import Data.Tuple (Tuple (..))
 import Data.URI (Authority (..), Host (..), Port (..))
 import ZeroMQ (registerProducer, registerWorker)
+import Node.Buffer (fromArray, toArray)
 
 
 socket :: Authority
@@ -16,10 +17,12 @@ socket = Authority Nothing [Tuple (IPv4Address "127.0.0.1") (Just (Port 3000))]
 
 producer :: Eff _ Unit
 producer = do
-  registerProducer socket $ \send ->
-    send "sup"
+  registerProducer socket $ \send -> do
+    b <- fromArray [73,75,70]
+    send b
 
 worker :: Eff _ Unit
 worker =
-  registerWorker socket $ \message ->
-    log message
+  registerWorker socket $ \message -> do
+    a <- toArray message
+    log $ show a
