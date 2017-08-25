4fccbe3e5063f9f1564403f5ee3388502d22d81d
Athan Clark <athan.clark@gmail.com>
2017-08-25 12:21:58 -0600

    mer

--- src/ZeroMQ.js
+++ src/ZeroMQ.js
@@ -22,10 +22,10 @@ exports.registerWorkerImpl = function registerWorkerImpl (socket, f) {
 exports.registerPublisherImpl = function registerPublisherImpl (socket, f) {
   var sock = zmq.socket('pub');
   sock.bindSync(socket);
-  var r = f(function sendImpl (channel,message) {
+  f(function sendImpl (channel,message) {
     sock.send([channel,message]);
   });
-  console.log(r,"?");
+  sock.send(["foo","asdf"]);
 };
 
 exports.registerSubscriberImpl = function registerSubscriberImpl (socket, channel, f) {
