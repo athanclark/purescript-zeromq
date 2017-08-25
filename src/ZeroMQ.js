9b22d4b0fb69355ae5c218aa351f60ddc07f5598
Athan Clark <athan.clark@gmail.com>
2017-08-25 11:29:30 -0600

    most bindings

---
+++ src/ZeroMQ.js
@@ -0,0 +1,37 @@
+"use strict";
+
+var zmq = require('zeromq');
+
+
+exports.registerProducerImpl = function registerProducerImpl (socket, f) {
+  var sock = zmq.socket('push');
+  sock.bindSync(socket);
+  f(function sendImpl (message) {
+    sock.send(message);
+  });
+};
+
+exports.registerWorkerImpl = function registerWorkerImpl (socket, f) {
+  var sock = zmq.socket('pull');
+  sock.connect(socket);
+  sock.on('message', function onMessageImpl (msg) {
+    f(msg);
+  });
+};
+
+exports.registerPublisherImpl = function registerPublisherImpl (socket, f) {
+  var sock = zmq.socket('pub');
+  sock.bindSync(socket);
+  f(function sendImpl (channel,message) {
+    sock.send([channel,message]);
+  });
+};
+
+exports.registerSubscriberImpl = function registerSubscriberImpl (socket, channel, f) {
+  var sock = zmq.socket('sub');
+  sock.connect(socket);
+  sock.subscribe(channel);
+  sock.on('message', function onMessageImpl (topic, msg) {
+    f(msg);
+  });
+};
---
+++ src/ZeroMQ.purs
@@ -0,0 +1,88 @@
+module ZeroMQ
+  ( ZEROMQ, registerProducer, registerWorker, registerPublisher, registerSubscriber
+  ) where
+
+import Prelude
+import Data.Maybe (Maybe (..))
+import Data.URI (Authority, URI (..), HierarchicalPart (..), Scheme (..))
+import Data.URI.URI as URI
+import Control.Monad.Eff (Eff, kind Effect)
+import Control.Monad.Eff.Uncurried (EffFn1, EffFn2, EffFn3, runEffFn2, mkEffFn1, runEffFn1, runEffFn3)
+
+
+foreign import data ZEROMQ :: Effect
+
+foreign import registerProducerImpl :: forall eff. EffFn2 (zeromq :: ZEROMQ | eff)
+                 String
+                 ( EffFn1 (zeromq :: ZEROMQ | eff)
+                     (EffFn1 (zeromq :: ZEROMQ | eff) String Unit)
+                     Unit
+                 )
+                 Unit
+
+registerProducer :: forall eff
+                  . Authority
+                 -> ( (String -> Eff (zeromq :: ZEROMQ | eff) Unit)
+                   -> Eff (zeromq :: ZEROMQ | eff) Unit
+                    )
+                 -> Eff (zeromq :: ZEROMQ | eff) Unit
+registerProducer authority f =
+  runEffFn2 registerProducerImpl (URI.print uri) (mkEffFn1 $ f <<< runEffFn1)
+  where
+    h = HierarchicalPart (Just authority) Nothing
+    uri = URI (Just (Scheme "tcp")) h Nothing Nothing
+
+foreign import registerWorkerImpl :: forall eff. EffFn2 (zeromq :: ZEROMQ | eff)
+                 String
+                 ( EffFn1 (zeromq :: ZEROMQ | eff) String Unit
+                 )
+                 Unit
+
+registerWorker :: forall eff
+                . Authority
+               -> (String -> Eff (zeromq :: ZEROMQ | eff) Unit)
+               -> Eff (zeromq :: ZEROMQ | eff) Unit
+registerWorker authority f =
+  runEffFn2 registerWorkerImpl (URI.print uri) (mkEffFn1 f)
+  where
+    h = HierarchicalPart (Just authority) Nothing
+    uri = URI (Just (Scheme "tcp")) h Nothing Nothing
+
+
+foreign import registerPublisherImpl :: forall eff. EffFn2 (zeromq :: ZEROMQ | eff)
+                 String
+                 ( EffFn1 (zeromq :: ZEROMQ | eff)
+                     (EffFn2 (zeromq :: ZEROMQ | eff) String String Unit)
+                     Unit
+                 )
+                 Unit
+
+registerPublisher :: forall eff
+                   . Authority
+                  -> ( (String -> String -> Eff (zeromq :: ZEROMQ | eff) Unit)
+                    -> Eff (zeromq :: ZEROMQ | eff) Unit
+                     )
+                  -> Eff (zeromq :: ZEROMQ | eff) Unit
+registerPublisher authority f =
+  runEffFn2 registerPublisherImpl (URI.print uri) (mkEffFn1 $ f <<< runEffFn2)
+  where
+    h = HierarchicalPart (Just authority) Nothing
+    uri = URI (Just (Scheme "tcp")) h Nothing Nothing
+
+foreign import registerSubscriberImpl :: forall eff. EffFn3 (zeromq :: ZEROMQ | eff)
+                 String
+                 String
+                 ( EffFn1 (zeromq :: ZEROMQ | eff) String Unit
+                 )
+                 Unit
+
+registerSubscriber :: forall eff
+                    . Authority
+                   -> String
+                   -> (String -> Eff (zeromq :: ZEROMQ | eff) Unit)
+                   -> Eff (zeromq :: ZEROMQ | eff) Unit
+registerSubscriber authority channel f =
+  runEffFn3 registerSubscriberImpl (URI.print uri) channel (mkEffFn1 f)
+  where
+    h = HierarchicalPart (Just authority) Nothing
+    uri = URI (Just (Scheme "tcp")) h Nothing Nothing
